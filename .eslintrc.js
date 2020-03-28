c587abd2da3b78b5ddaf22023abd6a770c03c588
Manu Phatak <manu.phatak@procore.com>
2020-03-28 11:02:04 -0700

    setup linter

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  root: true,
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: "module",
+    tsconfigRootDir: __dirname,
+    project: ["./tsconfig.json"],
+  },
+  plugins: ["@typescript-eslint"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    "plugin:prettier/recommended",
+  ],
+  rules: {
+    "@typescript-eslint/no-empty-function": "warn",
+    "prettier/prettier": "warn",
+  },
+}
